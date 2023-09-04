126b6dd2d2e72db9e658ead8799bfa2fff037eca
TranHai2001 <t.hai912001@gmail.com>
2023-09-04 09:17:02 +0700

    Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.example.pojo.AccountService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("xlmconfiguration/beans.xml");
+        AccountService accountService = applicationContext.getBean("accountService", AccountService.class);
+
+        System.out.println("Before money transfer");
+        System.out.println("Account 1 balance :" + accountService.getAccount(1).getBalance());
+        System.out.println("Account 2 balance :" + accountService.getAccount(2).getBalance());
+
+        accountService.transferMoney(1,2,5.0);
+        System.out.println("==============");
+        System.out.println("After money transfer");
+        System.out.println("Account 1 balance :" + accountService.getAccount(1).getBalance());
+        System.out.println("Account 2 balance :" + accountService.getAccount(2).getBalance());
+    }
+}
