952b9ea416c15a59405fb05d01eab3118417b8fc
TranHai2001 <t.hai912001@gmail.com>
2023-09-04 09:23:58 +0700

    update

--- src/main/java/org/example/javabasedconfiguration/Main.java
+++ src/main/java/org/example/javabasedconfiguration/Main.java
@@ -13,7 +13,7 @@ public class Main {
         System.out.println("Account 2 balance :" + accountService.getAccount(2).getBalance());
 
         accountService.transferMoney(1,2,5.0);
-        System.out.println("=================");
+        System.out.println("================");
         System.out.println("After money transfer");
         System.out.println("Account 1 balance :" + accountService.getAccount(1).getBalance());
         System.out.println("Account 2 balance :" + accountService.getAccount(2).getBalance());
